f793e9ba7c8336995fc71d8c0597e67128bf75bf
Sebastian Weddmark Olsson <visnae@gmail.com>
2014-03-10 22:48:50 +0100

    strange elisp-slime-nav error

--- init/init-elisp.el
+++ init/init-elisp.el
@@ -2,7 +2,7 @@
 (add-hook 'emacs-lisp-mode-hook 'setup-elisp)
 
 (defun setup-elisp ()
-  (package-install 'elisp-slime-nav)
+;;  (package-install 'elisp-slime-nav)
   (package-install 'rainbow-delimiters)
   (require 'elisp-slime-nav)
   (require 'rainbow-delimiters)
