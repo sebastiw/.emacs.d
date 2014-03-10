e9c53f815e066a90985117dbad87f4b3f5e01e08
Sebastian Weddmark Olsson <visnae@gmail.com>
2014-03-11 00:14:15 +0100

    elist-mode seems to have some stressful symtoms on emacs start

--- init/init-elisp.el
+++ init/init-elisp.el
@@ -1,12 +1,10 @@
 (install-package 'elisp-slime-nav)
 (require 'elisp-slime-nav)
 
-(defun setup-elisp ()
-  (install-package 'rainbow-delimiters)
-  (require 'rainbow-delimiters)
-  (rainbow-delimiters-mode t))
+(install-package 'rainbow-delimiters)
+(require 'rainbow-delimiters)
 
 (add-hook 'emacs-lisp-mode-hook 'elisp-slime-nav-mode)
-(add-hook 'emacs-lisp-mode-hook 'setup-elisp)
+(add-hook 'emacs-lisp-mode-hook 'rainbow-delimiters-mode)
 
 (provide 'init-elisp)
