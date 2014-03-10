908f1ee748e9f7272279afba00f7eba377cbf336
Sebastian Weddmark Olsson <visnae@gmail.com>
2014-03-11 00:02:14 +0100

    elisp-mode should now work

--- init/init-elisp.el
+++ init/init-elisp.el
@@ -1,19 +1,12 @@
-(defmacro after-load (feature &rest body)
-  "After FEATURE is loaded, evaluate BODY."
-  (declare (indent defun))
-  `(eval-after-load ,feature
-     '(progn ,@body)))
 
 (defun setup-elisp ()
   (install-package 'elisp-slime-nav)
   (install-package 'rainbow-delimiters)
-  (require 'elisp-slime-nav)
   (require 'rainbow-delimiters)
+  (rainbow-delimiters-mode t)
 
-  (elisp-slime-nav-mode)
-  (rainbow-delimiters-mode))
+  (elisp-slime-nav-mode))
 
-(after-load 'emacs-lisp-mode
-  (add-hook 'emacs-lisp-mode-hook 'setup-elisp))
+(add-hook 'emacs-lisp-mode-hook 'setup-elisp)
 
 (provide 'init-elisp)
