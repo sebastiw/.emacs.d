2184ae6d2e33c4ca2a0e36680ee6bbcb49885b9d
Sebastian Weddmark Olsson <visnae@gmail.com>
2014-09-15 10:44:22 +0200

    updated erlang-mode settings, edts is now in elpa/marmelade

--- init/init-packages.el
+++ init/init-packages.el
@@ -12,7 +12,7 @@
 
 (require 'package)
 (setq package-archives '(("gnu"       . "http://elpa.gnu.org/packages/")
-;                         ("marmalade" . "http://marmalade-repo.org/packages/")
+                         ("marmalade" . "http://marmalade-repo.org/packages/")
                          ("melpa"     . "http://melpa.milkbox.net/packages/")))
 
 (unless (file-exists-p  (concat emacs-dir "/" elpa-dir))
@@ -33,7 +33,7 @@
   (unless (package-installed-p pkg)
     (progn
       (unless (assoc pkg package-archive-contents)
-	(package-refresh-contents))
+        (package-refresh-contents))
       (package-install pkg))))
 
 (provide 'init-packages)
