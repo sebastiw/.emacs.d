283311d701c2b269ef534ea2c03cd714b354d32c
Sebastian Weddmark Olsson <visnae@gmail.com>
2014-09-19 16:39:46 +0200

    Added org-mode package-archive

--- init/init-packages.el
+++ init/init-packages.el
@@ -13,7 +13,8 @@
 (require 'package)
 (setq package-archives '(("gnu"       . "http://elpa.gnu.org/packages/")
                          ("marmalade" . "http://marmalade-repo.org/packages/")
-                         ("melpa"     . "http://melpa.milkbox.net/packages/")))
+                         ("melpa"     . "http://melpa.milkbox.net/packages/")
+                         ("org"       . "http://orgmode.org/elpa/")))
 
 (unless (file-exists-p  (concat emacs-dir "/" elpa-dir))
   (message "The directory %s does not exist, creating it." elpa-dir)
